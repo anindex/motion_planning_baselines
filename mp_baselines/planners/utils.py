887c3af4352ebdfec373fb55f2f14c62a96b48b2
Joao <joao@robot-learning.de>
2023-02-23 21:13:41 +0100

    smothness costs

--- mp_baselines/planners/utils.py
+++ mp_baselines/planners/utils.py
@@ -116,6 +116,25 @@ def get_trajs_free_coll(trajs, env):
     return trajs_free, trajs_coll
 
 
+def compute_path_length(trajs, env):
+    assert trajs.ndim == 3  # batch, horizon, state_dim
+    trajs_pos = env.get_q_position(trajs)
+    path_length = torch.linalg.norm(torch.diff(trajs_pos, dim=-2), dim=-1).sum(-1)
+    return path_length
+
+
+def compute_smothness(trajs, env):
+    assert trajs.ndim == 3
+    if trajs.shape[-1] == env.q_n_dofs:
+        # if there is no velocity information in the trajectory, compute it via finite difference
+        trajs_pos = env.get_q_position(trajs)
+        trajs_vel = torch.diff(trajs_pos, dim=-2)
+    else:
+        trajs_vel = env.get_q_velocity(trajs)
+    smoothness = torch.linalg.norm(torch.diff(trajs_vel, dim=-2), dim=-1).mean(-1)  # mean over trajectory horizon
+    return smoothness
+
+
 def smoothen_trajectory(traj, traj_len=30, tensor_args=None):
     traj = to_numpy(traj)
     try:
